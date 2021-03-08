cefac03f85bb52de086728873d91bcce4dc6bae8
Joe Moon <moonjose@gmail.com>
2021-03-07 20:34:06 -0600

    Add item complete; Quantity updates; navbar summary implemented

--- cart.js
+++ cart.js
@@ -41,8 +41,12 @@ Final Project
 
 "use strict";
 
+const itemsNum = document.getElementById("itemsNum");
+const subtotal = document.getElementById("subtotal");
+const deliveryfee = document.getElementById("deliveryfee");
+const total = document.getElementById("total");
 const inventoryWindow = document.getElementById("inventoryWindow");
-const displayCart = document.getElementById("shoppingCart");
+const cartWindow = document.getElementById("cartWindow");
 let inventoryItems; // nodelist of displayed inventory items
 let shoppingCart = []; // object array of items in shopping cart
 
@@ -132,62 +136,69 @@ function addItem(productID) {
   if (!inCart(productID)) {
     const selectedItem = inventory[productID]; // item reference call
 
-    // ITEM INFORMATION DISPLAY
+    // DIV that holds each item (row) in shopping cart
     let itemDiv = document.createElement("div");
     itemDiv.id = selectedItem.productID;
-    itemDiv.className = "cartDisplay";
+    itemDiv.className = "cartDisplay"; // 5 column grid
 
+    // Image - 1fr
     let selectedImage = document.createElement("img");
     selectedImage.src = selectedItem.img_src;
     selectedImage.alt = selectedItem.description;
     itemDiv.append(selectedImage);
 
+    // Description - 1fr
     let selectedName = document.createElement("p");
     selectedName.className = "mobileDisp";
     selectedName.textContent = selectedItem.name;
     itemDiv.append(selectedName);
 
+    // Price - 1fr
     let selectedPrice = document.createElement("p");
     selectedPrice.textContent = `$${selectedItem.price}`;
     itemDiv.append(selectedPrice);
 
-    // ITEM QUANITY SELECTION
-    let itemQuantity = document.createElement("div");
-    itemQuantity.className = "cartDisplay";
-
+    // Quantity - 1fr
+    let quantitySpan = document.createElement("span");
     let itemLabel = document.createElement("label");
     itemLabel.htmlFor = `product${productID}`;
     itemLabel.textContent = "Quantity: ";
-    itemQuantity.append(itemLabel);
+    quantitySpan.append(itemLabel);
 
     let itemQuant = document.createElement("input");
+    itemQuant.className = "inputField";
     itemQuant.id = `product${productID}`;
     itemQuant.name = `product${productID}`;
     itemQuant.type = "number";
     itemQuant.min = 0;
     itemQuant.value = 1;
-    itemQuantity.append(itemQuant);
+    quantitySpan.append(itemQuant);
+    itemDiv.append(quantitySpan);
 
-    // ITEM TOTAL CALCULATION
-    let itemTotal = document.createElement("div");
-    itemTotal.id = `product${productID}price`;
-    itemTotal.className = "cartDisplay";
+    let removeButton = document.createElement("button");
+    removeButton.id = `button${productID}`;
+    removeButton.className = "removeButtons";
+    removeButton.textContent = "Remove";
+    itemDiv.append(removeButton);
 
+    // Total Calculation - 1fr
     let itemTotalPrice = document.createElement("p");
-    itemTotalPrice.textContent = `$${selectedItem.price * itemQuant.value}`;
-    itemTotal.append(itemTotalPrice);
+    itemTotalPrice.id = `product${productID}price`;
+    itemTotalPrice.textContent = `$${(
+      selectedItem.price * itemQuant.value
+    ).toFixed(2)}`;
+    itemDiv.append(itemTotalPrice);
 
     cartWindow.append(itemDiv);
-    cartWindow.append(itemQuantity);
-    cartWindow.append(itemTotal);
 
     // Push shopping cart meta data to shoppingCart array
     const metadata = {
       productID: productID,
-      itemPrice: selectedItem.price,
-      itemQuantity: itemQuant.value,
+      itemPrice: selectedItem.price, // fixed
+      itemQuantity: itemQuant.value, // only value that changes
     };
     shoppingCart.push(metadata);
+    updateQuantity(productID, itemQuant.value);
   } else {
     // Item already in cart.  Get index of reselected item from shoppingCart array
     const cartIndex = shoppingCart.findIndex((meta) => {
@@ -200,12 +211,13 @@ function addItem(productID) {
     cartQuantity.value = newQuantity;
 
     let cartPrice = document.getElementById(`product${productID}price`);
-    let newPrice = cartQuantity.value * shoppingCart[cartIndex].itemPrice;
-    cartPrice.textContent = newPrice;
+    let newPrice = newQuantity * shoppingCart[cartIndex].itemPrice;
+    cartPrice.textContent = `$${newPrice.toFixed(2)}`;
 
     // Update meta data
-    shoppingCart[cartIndex].itemPrice = newPrice;
     shoppingCart[cartIndex].itemQuantity = newQuantity;
+
+    updateQuantity(productID, newQuantity);
   }
 }
 
@@ -216,20 +228,74 @@ function inCart(productID) {
   });
 }
 
+// updateQuantity: registers changes in item quantity
+function updateQuantity(productID, newQuantity) {
+  let cartIndex = shoppingCart.findIndex((item) => {
+    return item.productID.indexOf(`${productID}`) > -1;
+  });
+  shoppingCart[cartIndex].itemQuantity = newQuantity;
+  updatePrice(productID, cartIndex);
+}
+
+// updatePrice: recalculates and displays price changes
+function updatePrice(productID, cartIndex) {
+  // Update shopping cart item prices
+  const cartPrice = document.getElementById(`product${productID}price`);
+  const updatedQuantity = shoppingCart[cartIndex].itemQuantity;
+  const itemPrice = shoppingCart[cartIndex].itemPrice;
+  cartPrice.textContent = `$${updatedQuantity * itemPrice}`;
+
+  // Output Number of Items
+  const calcItemNum = shoppingCart.reduce((sum, itemSum) => {
+    return sum + +itemSum.itemQuantity;
+  }, 0);
+  itemsNum.textContent = calcItemNum;
+
+  // Output subtotal
+  const calcSubtotal = shoppingCart.reduce((sum, itemSum) => {
+    return sum + itemSum.itemQuantity * itemSum.itemPrice;
+  }, 0);
+
+  subtotal.textContent = `$${calcSubtotal.toFixed(2)}`;
+}
+
 // Remove: Add functionality so that a user may also remove an item from the shopping cart.
-function removeItem() {}
+function removeItem(productID) {
+  console.log(productID);
+}
 
 displayInventory();
 
-// EVENT LISTENERS
-
+// ***** EVENT LISTENERS *****
+// Listens for actions on the display window
 inventoryItems.forEach((item) => {
   item.addEventListener(
     "click",
     () => {
       addItem(item.id);
     },
-
     false
   );
 });
+
+// Listens for quantity changes in the shopping cart list
+cartWindow.addEventListener(
+  "change",
+  (e) => {
+    if (e.target.classList.contains("inputField")) {
+      updateQuantity(e.target.id.slice(-1), e.target.value);
+    }
+  },
+  false
+);
+
+// Listens for item Remove actions
+cartWindow.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.classList.contains("removeButtons")) {
+      removeItem(e.target.parentElement.id);
+    }
+  },
+  false
+);
